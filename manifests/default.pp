3856ea1eab2970e2d1d38509f8cc1e904d395d16
Catalin Muresan <muresan@pythian.com>
2015-11-19 14:06:52 +0000

    added puppet module parameters to setup the clister and add ui

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,19 +1,49 @@
 
 ## ES nodes
 
-node /^es\d+/ {
+node /^el\d+/ {
   package { "vim-enhanced": ensure => 'installed', }
   package { "git"         : ensure => 'installed', }
   package { "acpid"       : ensure => 'installed', }
+  package { "nss"         : ensure => 'latest',    }
 
 
   class { 'elasticsearch': 
-    config => { 'cluster.name' => 'es-cluster' }
+    manage_repo    => true,
+    java_install   => true,
+    repo_version   => '2.0',
+    config => {
+      'cluster.name'             => 'es-cluster',
+      'index.number_of_replicas' => '1',
+      'index.number_of_shards'   => '3',
+      'network.host'             => $::ipaddress,
+      'http.cors.enabled'        => true,
+    }
   }
 
-  elasticsearch::instance { $::hostname:
-    datadir => "/var/lib/es-data-${::hostname}",
+  elasticsearch::instance { "es01":
+    datadir        => "/var/lib/es-data-$hostname",
   }
+
+  elasticsearch::instance { "$hostname":
+    ensure         => absent
+  }
+
+  elasticsearch::template { "template":
+    ensure         => absent
+  #  content        => '{"template":"*","settings":{"number_of_replicas":1}}',
+  #  host           => $::ipaddress,
+  #  port           => 9200
+  }
+
+  elasticsearch::plugin { 'royrusso/elasticsearch-hq/v2.0.3':
+    instances      => 'es01'
+  }
+
+  elasticsearch::plugin { 'lmenezes/elasticsearch-kopf/2.0':
+    instances      => 'es01'
+  }
+
 }
 
 
